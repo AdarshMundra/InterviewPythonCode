c54fc860c7df86e47b59c768b0b1593ff30b55f0
adarshmundra <adarsh.mundra1305@gmail.com>
2023-09-21 16:17:49 +0530

    Final first code

--- P1.py
+++ P1.py
@@ -2,8 +2,32 @@
 element = int(input("elter a integer"))
 
 if element>0:
-    final_answer = str(element)[::-1]
-    print(final_answer)
+    sign =1
+else:
+    sign =-1
+
+final_answer = str(abs(element))[::-1]
+print(int(final_answer)*sign)
+
+
+def reverse_integer(n):
+    reversed_num = 0
+    sign = -1 if n < 0 else 1
+    n = abs(n)
+    while n != 0:
+        last_digit = n % 10
+        reversed_num = reversed_num * 10 + last_digit
+        n = n // 10
+
+    return reversed_num * sign
+
+# Input integer
+num = int(input("Enter an integer: "))
+
+# Reverse the integer
+reversed_num = reverse_integer(num)
+
+print("Reversed integer:", reversed_num)
 
 
 
